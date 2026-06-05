8e6c2f1f868de892c3673800cc50dbfd9208f6bc
Rushanksavant <rushanksavant34@gmail.com>
2026-06-05 20:21:53 +0530

    env vars for hf-space

--- settings.py
+++ settings.py
@@ -4,9 +4,17 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file = ".env", extra = "ignore")
 
+    QDRANT_CLOUD_CLUSTER_ENDPOINT: SecretStr 
+    QDRANT_CLOUD_KEY: SecretStr
+    HF_API_KEY: SecretStr
+    
     GEMINI_API_KEY: SecretStr ## doesn't reveal on print
+    
+    MCP_SECRET_KEY: SecretStr 
+    SERVER_URL: str
+    
     ENV: str
 
 
 settings = Settings()
-print(settings.GEMINI_API_KEY)
+# print(settings.GEMINI_API_KEY)
