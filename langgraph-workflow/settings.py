f44e97decfd5256118e29ef71e2d77c212aa3c46
Rushanksavant <rushanksavant34@gmail.com>
2026-06-10 00:10:25 +0530

    folder isolation

---
+++ langgraph-workflow/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file = ".env", extra = "ignore")
+    
+    GEMINI_API_KEY: SecretStr 
+    MCP_SECRET_KEY: SecretStr 
+    SERVER_URL: str
+
+    GROQ_API_KEY: SecretStr
+    
+    max_input_chars: int = 1000  # User query should be at max 250 tokens
+
+
+
+settings = Settings()
+# print(settings.GEMINI_API_KEY)
