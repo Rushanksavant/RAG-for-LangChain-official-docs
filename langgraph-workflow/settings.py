540923b908d5cd0253351c84e49aefa4dcab6e36
Rushanksavant <rushanksavant34@gmail.com>
2026-06-16 01:31:28 +0530

    adding langsmith

--- langgraph-workflow/settings.py
+++ langgraph-workflow/settings.py
@@ -9,6 +9,9 @@ class Settings(BaseSettings):
     SERVER_URL: str
 
     GROQ_API_KEY: SecretStr
+
+    LANGSMITH_API_KEY: SecretStr
+    
     
     max_input_chars: int = 1000  # User query should be at max 250 tokens
 
